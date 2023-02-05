ab7c263be2b0866dba5da334bdb70058a37e01fa
Dominic Ojeda <dominicojeda.coding@gmail.com>
2023-02-05 14:04:47 -0700

    highscore now saves.

--- Scripts/State.gd
+++ Scripts/State.gd
@@ -14,8 +14,12 @@ signal bad_teeth_updated(num)
 signal pulling
 signal end_game
 
+var save_path: String = "user://sharkData.txt"
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+  load_game_data()
+  print('currentHiS:', high_score)
   pass # Replace with function body.
 
 func add_bad_teeth(amount):
@@ -25,6 +29,7 @@ func add_bad_teeth(amount):
     game_over = true
     if score > high_score:
       high_score = score
+      save_game_data()
 #    print("--GAME OVER--")
     emit_end_game()
 
@@ -46,6 +51,34 @@ func emit_pulling():
 
 func emit_end_game():
   emit_signal("end_game")
+
+func save_game_data():
+  var save_file = File.new()
+  save_file.open(save_path, File.WRITE)
+  var new_data = to_json({"high_score": high_score})
+  print(new_data)
+  save_file.store_line(new_data)
+  save_file.close()
+  print('Save Data: ', high_score)
+  pass
+
+func load_game_data():
+  var load_file = File.new()
+  if not load_file.file_exists(save_path):
+    return
+  load_file.open(save_path, File.READ)
+  var node_data = parse_json(load_file.get_line())
+  high_score = node_data["high_score"]
+  load_file.close()
+  print('Data Loaded')
+  pass
+
+func reset_data():
+  print('Reset Game Data')
+  high_score = 0
+  save_game_data()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#  pass
+#	pass
+
